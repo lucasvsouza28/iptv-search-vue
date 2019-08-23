27b4db0ef25ca28304290effc8d42f8e57ad21b8
Lucas Souza <lucasvsouza28@gmail.com>
2019-08-23 15:09:51 -0300

    Update program.ts

--- api/src/program.ts
+++ api/src/program.ts
@@ -1,6 +1,6 @@
 import { StartUpInstance } from './startup';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 StartUpInstance.App.listen(port, () => {
     // console.log('API listenig on port ' + port);
 });
