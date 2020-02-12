3129d3a2c910f280b5a3ec5e6f6946a0a793982c
Lucas Souza <lucas.souza@i4pro.com.br>
2020-02-12 10:28:36 -0300

    - Implementando proxy - Ajuste no layout

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,36 +1,38 @@
 <template>
-    <v-row align="center" justify="center" v-if="!data || (data && !data.groups.length)">
-        <v-icon>sentiment_dissatisfied</v-icon> Nenhum item encontrado
-    </v-row>
-    <v-progress-circular v-else-if="loading" indeterminate="true"></v-progress-circular>
-    <div style="width: 100%;" v-else>
-        <v-row v-for="(group, i) in data.groups" :key="`group-${i}`">
-        <v-subheader class="headline">{{ group.groupName }}</v-subheader>
-        <v-container fluid>
-            <v-row>
+    <v-row>
+        <v-col align="center" justify="center" v-if="!data || (data && !data.groups.length)">
+            <v-icon>sentiment_dissatisfied</v-icon> Nenhum item encontrado
+        </v-col>
+        <v-progress-circular v-else-if="loading" indeterminate="true"></v-progress-circular>        
+        <v-col style="width: 100%;" v-else>
+            <v-row v-for="(group, i) in data.groups" :key="`group-${i}`">
+                <v-subheader class="headline">{{ group.groupName }}</v-subheader>
+                <v-container fluid>
+                    <v-row>
 
-                <v-card
-                    class="d-inline-flex ml-4"
-                    v-for="(item, j) in group.Items" :key="`item-${j}`"
-                    width="200px"
-                    tile
-                >
-                    <v-img
-                    v-if="item.imgUrl"
-                    class="white--text"
-                    gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-                    height="100px"
-                    v-bind:src="item.imgUrl"
-                    >
-                    <v-card-title class="align-end fill-height">{{ item.title }}</v-card-title>
-                    </v-img>                    
-                </v-card>
+                        <v-card
+                            class="d-inline-flex ml-4"
+                            v-for="(item, j) in group.Items" :key="`item-${j}`"
+                            width="200px"
+                            tile
+                        >
+                            <v-img
+                            v-if="item.imgUrl"
+                            class="white--text"
+                            gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+                            height="100px"
+                            v-bind:src="item.imgUrl"
+                            >
+                            <v-card-title class="align-end fill-height">{{ item.title }}</v-card-title>
+                            </v-img>                    
+                        </v-card>
 
+                    </v-row>
+                    <v-divider style="margin-top: 20px"></v-divider>
+                </v-container>
             </v-row>
-            <v-divider style="margin-top: 20px"></v-divider>
-            </v-container>
-        </v-row>
-    </div>
+        </v-col>
+    </v-row>
 </template>
 
 <script lang="ts">
