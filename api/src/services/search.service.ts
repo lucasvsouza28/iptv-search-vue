acc2f59a44a89cc69d9a4b5a84264b602af5b54c
Lucas Souza <lucasvsouza28@gmail.comm>
2020-09-19 19:49:50 -0300

    Adicionando logs

--- api/src/services/search.service.ts
+++ api/src/services/search.service.ts
@@ -22,7 +22,12 @@ export class SearchService {
         const content = await readFile(filePath, 'utf8');
 
         if (content) {
+            console.log('content found: ', content.substring(0, 30));
+
             ret = this.parseContent(query, content);
+        }
+        else{
+            console.log('content NOT found');
         }
         
         return ret;
@@ -37,8 +42,11 @@ export class SearchService {
         return ret;
     }
 
-    private parseContent(query: string, content: string): ResultGroupModel[] {
+    private parseContent(query: string, content: string): ResultGroupModel[] {        
         const lines = content.split('\n');
+
+        console.log('lines count', lines.length);
+
         let dicGroups: {[Key: string]: ResultGroupModel} = {};
         const rgx = new RegExp('\#extinf.+tvg-logo\=\"(?<logo>.+)\".+group-title\=\"(?<group>.+)\"\,(?<name>.+)', 'gmi');
 
@@ -54,24 +62,8 @@ export class SearchService {
                     dicGroups[match.groups.group].Items = dicGroups[match.groups.group].Items || [];
                     dicGroups[match.groups.group].Items.push(new ResultModel(match.groups.name, match.groups.group, match.groups.logo));
                 }
-            }            
-            // if (line.indexOf('#EXTINF') >= 0) {
-            //     const arrTvg = line.split('tvg-');
-            //     const part2 = arrTvg[arrTvg.length-1];
-            //     const arrGt = part2.split('group-title')[1];
-            //     const group = arrGt.replace("=", "").split(',')[0].replace("\"", "").replace("\"", "");
-            //     const name = arrTvg[2].replace('name=', '').replace('\"', '').replace("\"", "");
-            //     const logo = part2.split('group-title')[0].replace('logo=', '').replace('\"', '').replace("\"", "");
-                
-            //     const groupMatch = group.toUpperCase().indexOf(query.toUpperCase()) >= 0;
-            //     const nameMatch = name.toUpperCase().indexOf(query.toUpperCase()) >= 0;
+            }
 
-            //     if (groupMatch || nameMatch) {
-            //         ret[group] = ret[group] || new ResultGroupModel(group);
-            //         ret[group].Items = ret[group].Items || [];
-            //         ret[group].Items.push(new ResultModel(name, group, logo));
-            //     }
-            // }
         });
 
         let ret: ResultGroupModel[] = [];
